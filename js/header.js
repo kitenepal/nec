92e5086d341073df572e1e98b06f03a46cdd5b15
NidanStha <nidanshrestha@gmail.com>
2018-03-08 15:24:28 +0545

    added header

---
+++ js/header.js
@@ -0,0 +1,17 @@
+$( () => {
+	
+	//On Scroll Functionality
+	$(window).scroll( () => {
+		var windowTop = $(window).scrollTop();
+		windowTop > 70 ? $('nav').addClass('navshadow') : $('nav').removeClass('navshadow');
+		windowTop > 70 ? $('nav .container-fluid').css('padding-top','10px') : $('nav .container-fluid').css('padding-top','60px');
+	});
+
+	$(window).scroll(function(){
+		var wScroll = $(this).scrollTop();
+		$('.hero-text').css({
+			'transform'	: 'translate(0%, '+ wScroll/3 +'% )'
+		});
+	});
+
+});
