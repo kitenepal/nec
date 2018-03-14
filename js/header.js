e81b2bffbf2ac69327477dfec24c719ee62e7b90
NidanStha <nidanshrestha@gmail.com>
2018-03-14 20:44:54 +0545

    slight change

--- js/header.js
+++ js/header.js
@@ -5,6 +5,7 @@ $( () => {
 		var windowTop = $(window).scrollTop();
 		windowTop > 70 ? $('nav').addClass('navshadow') : $('nav').removeClass('navshadow');
 		windowTop > 70 ? $('nav .container-fluid').css('padding-top','10px') : $('nav .container-fluid').css('padding-top','60px');
+		windowTop > 70 ? $('.navbar-brand>img').attr('src','image/logow.png') : $('.navbar-brand>img').attr('src','image/logo.png');
 	});
 
 	$(window).scroll(function(){
