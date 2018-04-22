571e56b42d1ea927eb5fb8f54e65eae63528cfdf
NidanStha <nidanshrestha@gmail.com>
2018-04-22 14:45:32 +0545

    added in fotter

--- js/header.js
+++ js/header.js
@@ -4,7 +4,7 @@ $( () => {
 $(window).scroll( () => {
 	var windowTop = $(window).scrollTop();
 	windowTop > 70 ? $('nav').addClass('navshadow') : $('nav').removeClass('navshadow');
-	windowTop > 70 ? $('nav .container-fluid').css('padding-top','10px') : $('nav .container-fluid').css('padding-top','60px');
+	windowTop > 70 ? $('nav .navanime').css('padding-top','10px') : $('nav .navanime').css('padding-top','60px');
 	windowTop > 70 ? $('.navbar-brand>img').attr('src','image/logow.png') : $('.navbar-brand>img').attr('src','image/logo.png');
 });
 
